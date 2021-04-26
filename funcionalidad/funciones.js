6e33bdf84c7e133ee747a7c3fed17331fa3dd1f7
lissetmedel <lissetmedel@gmail.com>
2021-04-26 14:29:40 -0500

    Add files via upload

--- funcionalidad/funciones.js
+++ funcionalidad/funciones.js
@@ -11,3 +11,11 @@ function crear(){
 	document.getElementById('cuadrado').style.height = alto+"px";
 	document.getElementById('cuadrado').style.width = ancho+"px";
 }
+function menu(){
+	document.getElementById('menu').classList.toggle('visible');
+	let e=document.querySelectorAll('.contenedor');
+	let t=e.length;
+	for(i=t-1; i>=0;i--){
+		e[i].classList.toggle('size');
+	}
+}
